f30ba05cb6c3ab347cfeb533fbcdf3f34a1993a8
kataricode <lechiloi.24020@gmail.com>
2026-03-25 23:45:28 +0700

    Create CTX.py

---
+++ CTX.py
@@ -0,0 +1,4 @@
+# Đây là file cấu hình tạm thời để tránh lỗi import
+# Bạn có thể thêm các biến cần thiết vào đây nếu code yêu cầu
+prefix = "!"
+# Thêm các biến khác nếu bạn nhớ trong file gốc có gì
